ad9ac5bfd706f80f5a77c59d0e305ea020d878ab
sidhanathdon <137877586+sidhanathdon@users.noreply.github.com>
2023-12-13 21:18:20 +0530

    Update store.js

--- pages/store.js
+++ pages/store.js
@@ -1,10 +1,23 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 function Store() {
+  const router =useRouter();
+  useEffect(() => {
+    if (localStorage.getItem('loginStatus') !== 'true') {
+      router.replace('/login');
+    }
+  }, []);
+
+
+const handelLogout = () => {
+  localStorage.setItem('loginStatus', 'false');
+  router.replace('/login'); // Use replace instead of push
+};
+
   return (
     <div className='store'>
       <h2>Welcome to the store!</h2>
-      <button className='logout-btn'>Logout</button>
+      <button className='logout-btn'onClick={handelLogout}>Logout</button>
     </div>
   );
 }
