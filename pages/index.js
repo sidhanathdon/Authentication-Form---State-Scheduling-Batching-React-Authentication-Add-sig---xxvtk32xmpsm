e8f5ad507dc30ce6dd1359b2d5ea66b7357caad9
sidhanathdon <137877586+sidhanathdon@users.noreply.github.com>
2023-12-13 21:17:11 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,54 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
+import {useRouter} from "next/router";
 
 function Register() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(false);
+  const [loginStatus,setLoginStatus]=useState(false);
+
+  const router=useRouter();
+  useEffect(()=>{
+    localStorage.setItem('loginStatus',loginStatus);
+  },[loginStatus]);
+
+  const handleEmailChange=(e)=>{
+    setEmail(e.target.value);
+  }
+
+  const handlePassChange=(e)=>{
+    setPassword(e.target.value);
+  }
+
+  const handleSubmitChange=(e)=>{
+    e.preventDefault();
+    if (email.length < 1 || password.length < 1) {
+      setError(true);
+    } else {
+      localStorage.setItem('user', JSON.stringify({ email, password }));
+      setLoginStatus(true);
+      setError(false);
+      setEmail('');
+      setPassword('');
+      router.push('/login');
+    }
+  };
 
   return (
     <div className='register'>
       <h2>Register</h2>
-      <form className='sign-up-form'>
-        <p className='error-para'>"Email or password isn't entered!"</p>
+      <form className='sign-up-form' onSubmit={handleSubmitChange}>
+        {error &&
+        <p className='error-para'>"Email or password isn't entered!"</p>}
         <div className='email-div'>
           <label htmlFor='email'>Email: </label>
-          <input type='email' id='email' />
+          <input type='email' id='email'value={email} onChange={handleEmailChange}/>
         </div>
         <div className='password-div'>
           <label htmlFor='password'>Password: </label>
-          <input type='password' id='password' />
+          <input type='password' id='password' value={password} onChange={handlePassChange}/>
         </div>
         <button type='submit' className='register-btn'>
           Register
@@ -26,7 +56,7 @@ function Register() {
       </form>
       <div>
         Already have an account?
-        <Link href=''>
+        <Link href='/login'>
           <button className='login-link'>Login</button>
         </Link>
       </div>
