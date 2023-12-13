0b8794103ae6fe9225669bbd4885028331d65dbf
sidhanathdon <137877586+sidhanathdon@users.noreply.github.com>
2023-12-13 21:17:42 +0530

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,25 +1,50 @@
 'use client';
 import React, { useState } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const Login = () => {
+  const router=useRouter();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(false);
-  const [isLogIn, setIsLogIn] = useState(false);
+  // const [isLogIn, setIsLogIn] = useState(false);
 
+  const handleEmailChange=(e)=>{
+    setEmail(e.target.value);
+  }
+  const handlePassChange=(e)=>{
+    setPassword(e.target.value);
+  }
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    if (email.length < 1 || password.length < 1) {
+      setError(true);
+    } else {
+      const user = JSON.parse(localStorage.getItem('user'));
+      if (user && user.email === email && user.password === password) {
+        setError(false);
+        setEmail('');
+        setPassword('');
+        localStorage.setItem('loginStatus', 'true'); // Update the key to 'loginStatus'
+        router.push('/store');
+      } else {
+        setError(true);
+      }
+    }
+  };
   return (
     <div className='login'>
       <h2>Login</h2>
-      <form>
-        <p className='error-para'>"Email or password is invalid"</p>
+      <form onSubmit={handleSubmit}>
+        {error && <p className='error-para'>"Email or password is invalid"</p>}
         <div className='email-div'>
           <label htmlFor='email'>Email: </label>
-          <input type='email' id='email' />
+          <input type='email' id='email' value={email} onChange={handleEmailChange} />
         </div>
         <div className='password-div'>
           <label htmlFor='password'>Password: </label>
-          <input type='password' id='password' />
+          <input type='password' id='password' value={password} onChange={handlePassChange} />
         </div>
         <button type='submit' className='login-btn'>
           Log In
@@ -27,7 +52,7 @@ const Login = () => {
       </form>
       <div>
         Don't have an account?
-        <Link href=''>
+        <Link href='/'>
           <button className='register-link'>Register</button>
         </Link>
       </div>
